f25606070691abdabc52382d4c9d53a463474039
martimgalvao <26451660+martimgalvao@users.noreply.github.com>
2021-07-19 21:53:48 -0400

    Test Files: Jupyrer and Py

---
+++ test1_1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+x = 5;
+y = 10;
+
+def eq (x, y):
+    z = x + y;
+    return z;
+
+
+# In[12]:
+
+
+z = eq(x,y);
+
+
+# In[13]:
+
+
+if z == 15:
+    print("Addition Test Passed")
+    
+elif z != 15:
+    print("Addition Test Failed")
+
+
+# In[ ]:
+
+
+
+
